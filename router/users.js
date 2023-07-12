535c8ec1606a8cd332ba6cada97a861bfd60e97b
sachintha-chathuranga <mmschathuranga1@gmail.com>
2023-07-12 18:27:42 +0530

    change the order of logout route

--- router/users.js
+++ router/users.js
@@ -9,9 +9,9 @@ const router = express.Router();
 //if request comming to this end point these function will execute
 router.route('/signUp').post(validation, createUser);
 router.route('/login').post(loginUser);
+router.route('/logout').put(logoutUser);
 router.route('/friends').get(verifyJWT, getFriends);
 router.route('/search').post(verifyJWT, searhFriends);
-router.route('/logout').put(logoutUser);
 router.route('/').put(verifyJWT, updateValidation, updateUser).delete(verifyJWT, deleteUser);
 router.route('/:id').get(verifyJWT, getUser);
 
